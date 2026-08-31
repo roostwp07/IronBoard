a5c17be3f1fda7472a72c0dd1d42e687ce60629a
par <wangparmida@gmail.com>
2026-08-31 14:59:30 -0400

    add instructional comments

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './index.css'  // automatically apply CSS styles globally to the entire app
 import App from './App.tsx'
+
+/*
+* `document` is the whole page
+* `document.getElementById('root')` returns the empty container initialized in @index.html
+* `createRoot` gives React control of the container
+* .render(...) populates the container, in this case with whatever is displayed by invoking the App() function
+*/
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
